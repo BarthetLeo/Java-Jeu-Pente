1cb87d8e933b90ebf5f0d35754d52aa852433eb2
Lochungvu Maxence <maxence.lochungvu@etu.amu-univ.fr>
2021-11-29 21:56:38 +0100

    Corriger sur le affiche_grille de pente
    J'ai créer un rectangle dans lequel il y a le plateau avec le quadrilllage 19*19 et a coté un espace blanc pour le menu

--- Pente.java
+++ Pente.java
@@ -20,15 +20,18 @@ public class Pente extends JPanel{
     }
     public void affiche_grille(Graphics g)
     {
+        int WM_plat = (getWidth()/4)*3;
         //Convertissseur de Graphics en Graphics2D pour plus d'option.
         Graphics2D g2 = (Graphics2D)g;
+        g2.setColor(Color.red);
+        g2.drawRect(0, 0, WM_plat, getHeight());
         g2.setColor(Color.black);
-        //Boucle pour dessiner les lignes verticales
+        //Boucle pour dessiner les lignes verticales et des noms des lignes
         for(int i =1;i<20;i++)
-            g2.drawLine((getWidth()/20)*i, 0, (getWidth()/20)*i, getHeight());
+            g2.drawLine((WM_plat/20)*i, (getHeight()/20), (WM_plat/20)*i, (getHeight()/20)*19);
         //Boucle pour dessiner les lignes horizontales
         for(int i =1;i<20;i++)
-            g2.drawLine(0, (getHeight()/20)*i, getWidth(), (getHeight()/20)*i);
+            g2.drawLine((WM_plat/20), (getHeight()/20)*i, (WM_plat/20)*19, (getHeight()/20)*i);
     }
 
     public void paint(Graphics g)
